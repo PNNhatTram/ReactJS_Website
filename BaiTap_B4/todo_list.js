de3b196480942eb3cb7dbc8440615a928c71adde
PNNhatTram <nhattrampn782004@gmail.com>
2024-07-20 16:13:55 +0700

    add select status

--- BaiTap_B4/todo_list.js
+++ BaiTap_B4/todo_list.js
@@ -21,17 +21,32 @@ function addTask(task){
     //Nếu task đã hoàn thành, thêm lớp "completed" vào phần tử
     if(task.completed) taskItem.classList.toggle("completed");
 
-    //Thêm sự kiện click để đánh dấu task hoàn thành hoặc chưa hoàn thành.
+    /*//Thêm sự kiện click để đánh dấu task hoàn thành hoặc chưa hoàn thành.
     taskItem.addEventListener("click", function(){
         taskItem.classList.toggle("completed");
-    })
+    })*/
+
+    const statusSelect = document.createElement("select");
+    const options = [
+        {value: "pending", text: "Pending"},
+        {value: "inProgress", text: "In Progress"},
+        {value: "completed", text: "Completed"}
+    ];
+    for(let i = 0; i<options.length; i++)
+    {
+        const option = document.createElement("option");
+        option.value = options[i].value;
+        option.textContent = options[i].text;
+        statusSelect.appendChild(option);
+    }
 
     const removeButton = document.createElement("button");
     removeButton.textContent="Remove";
     removeButton.addEventListener("click", function(){
         taskList.removeChild(taskItem);
     })
-    
+
+    taskItem.appendChild(statusSelect);
     taskItem.appendChild(removeButton);
     taskList.appendChild(taskItem);
 }
@@ -59,3 +74,23 @@ if(tasks){
         addTask(task);
     }
 }
+
+//Thêm trường status cho mỗi task để đánh giá tiến độ của task theo 3 cấp độ: Pending, In Progress, Completed.
+/*function selectStatus(){
+    const select = document.createElement("select");
+    //tạo mảng các status
+    const options = [
+        {value: "pending", text: "Pending"},
+        {value: "inProgress", text: "In Progress"},
+        {value: "completed", text: "Completed"}
+    ];
+    //Thêm các option vào phần tử select
+    for(let i = 0; i<options.length; i++)
+    {
+        const optionElement = document.createElement("option");
+        optionElement.value = options[i].value;
+        optionElement.textContent = options[i].text;
+        selectElement.appendChild(optionElement);
+    }
+    return select
+}*/
