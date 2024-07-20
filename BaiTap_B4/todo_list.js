f9b78cde367c4b51e35893d0c09b3631d9e99f41
PNNhatTram <nhattrampn782004@gmail.com>
2024-07-20 16:48:34 +0700

    complete question 1

--- BaiTap_B4/todo_list.js
+++ BaiTap_B4/todo_list.js
@@ -1,13 +1,14 @@
 
-// Xử lý sự kiện khi nhập task
+/* XỬ LÝ SỰ KIỆN KHI NHẬP TASK */
 document.getElementById("formTask").onsubmit=function(event){
     event.preventDefault(); //tránh load lại trang
     const inputNode=document.getElementById("inputTask");
-    const taskText = inputNode.value.trim(); //Lấy giá trị từ ô nhập và loại bỏ khoảng trắng thừa ở đầu và cuối
+    //Lấy giá trị từ ô nhập và loại bỏ khoảng trắng thừa ở đầu và cuối
+    const taskText = inputNode.value.trim();
     if(taskText!==""){
         const task = {
             text: taskText,
-            completed: false
+            status: "pending"
         };
         addTask(task);
     }
@@ -18,14 +19,15 @@ function addTask(task){
     const taskItem = document.createElement("li");
     taskItem.textContent = task.text;
 
-    //Nếu task đã hoàn thành, thêm lớp "completed" vào phần tử
-    if(task.completed) taskItem.classList.toggle("completed");
+    /*//Nếu task đã hoàn thành, thêm lớp "completed" vào phần tử
+    if(task.status) taskItem.classList.toggle("completed");
 
-    /*//Thêm sự kiện click để đánh dấu task hoàn thành hoặc chưa hoàn thành.
+    //Thêm sự kiện click để đánh dấu task hoàn thành hoặc chưa hoàn thành.
     taskItem.addEventListener("click", function(){
         taskItem.classList.toggle("completed");
     })*/
-
+    
+    //Thêm select status
     const statusSelect = document.createElement("select");
     const options = [
         {value: "pending", text: "Pending"},
@@ -40,18 +42,25 @@ function addTask(task){
         statusSelect.appendChild(option);
     }
 
+    //Thêm button remove
     const removeButton = document.createElement("button");
     removeButton.textContent="Remove";
     removeButton.addEventListener("click", function(){
         taskList.removeChild(taskItem);
     })
 
+    //Thêm lớp trạng thái
+    statusSelect.addEventListener("onchange", function(){
+        task.status = statusSelect.value;
+    })
+    taskItem.classList.toggle(task.status);
+
     taskItem.appendChild(statusSelect);
     taskItem.appendChild(removeButton);
     taskList.appendChild(taskItem);
 }
 
-// Lưu danh sách task vào Local Storage
+/*LƯU DANH SÁCH TASK VÀO LOCAL STORAGE*/
 document.getElementById("saveBtn").addEventListener("click", function(event){
     event.preventDefault();
     const taskList = document.getElementById("tasks");
@@ -60,37 +69,17 @@ document.getElementById("saveBtn").addEventListener("click", function(event){
         const task = {
             text: taskItem.childNodes[0].textContent,
             //Kiểm tra xem phần tử có lớp "completed" không
-            completed: taskItem.classList.contains("completed")
+            status: taskItem.querySelector("select").value
         };
         tasks.push(task);
     }
     localStorage.setItem("tasks",JSON.stringify(tasks));
 })
 
-//Tải danh sách task từ Local Storage khi trang được tải
+/*TẢI DANH SÁCH TASK TỪ LOCAL STORAGE KHI TRANG ĐƯỢC TẢI*/
 const tasks = JSON.parse(localStorage.getItem("tasks"));
 if(tasks){
     for(let task of tasks){
         addTask(task);
     }
 }
-
-//Thêm trường status cho mỗi task để đánh giá tiến độ của task theo 3 cấp độ: Pending, In Progress, Completed.
-/*function selectStatus(){
-    const select = document.createElement("select");
-    //tạo mảng các status
-    const options = [
-        {value: "pending", text: "Pending"},
-        {value: "inProgress", text: "In Progress"},
-        {value: "completed", text: "Completed"}
-    ];
-    //Thêm các option vào phần tử select
-    for(let i = 0; i<options.length; i++)
-    {
-        const optionElement = document.createElement("option");
-        optionElement.value = options[i].value;
-        optionElement.textContent = options[i].text;
-        selectElement.appendChild(optionElement);
-    }
-    return select
-}*/
